fe3db41abe3a2e5e3fba476b9d2630757e657048
Жунусов Эльнур Ильдарович <elnur.zhunussov@ffin.life>
2023-09-29 16:12:14 +0600

    Stop: Finished the project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,62 @@
 <template>
-
+    <h1>My reaction timer</h1>
+    <button @click="start" :disabled='isPlaying'>Play</button>
+    <BlockVue v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <ResultsVue v-if="showResults" :reactionTime="reactionTime"/>
 </template>
 
 <script>
-export default {
+import BlockVue from './components/Block.vue';
+import ResultsVue from './components/Results.vue';
 
+export default {
+    name: 'App',
+    components: {BlockVue, ResultsVue},
+    data() {
+        return {
+            isPlaying: false,
+            delay: null,
+            reactionTime: null,
+            showResults: false,
+        }
+    },
+    methods: {
+        start() {
+            this.delay = 2000 + Math.random() * 5000;
+            this.isPlaying = true;
+            this.showResults = false;
+        },
+        endGame(reactionTime) {
+            this.reactionTime = reactionTime;
+            this.isPlaying = false;
+            this.showResults = true;
+        },
+    }
 }
 </script>
 
 <style>
+#app {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+}
 
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+}
+
+button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+}
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p class="description">Your reaction time is: <span class="time">{{ reactionTime }} ms</span></p>
+    <span class="rank">{{ rank }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["reactionTime"],
+  data() {
+    return {
+        rank: null,
+    }
+  },
+  mounted() {
+    if (this.reactionTime < 200) {
+        this.rank = 'Pro';
+    } else if (this.reactionTime < 300) {
+        this.rank = 'Mid';
+    } else {
+        this.rank = 'Noob';
+    };
+  }
+};
+</script>
+
+<style>
+.description {
+    font-size: 1em;
+    color: black;
+    font-weight: bold;
+}
+
+.time {
+    font-size: 1em;
+    color: red;
+    font-weight: bold;
+}
+
+.rank {
+    font-size: 1em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
